a51f5d2764e6a2bd6489983e55806b79728f5d8d
recca0120 <recca0120@gmail.com>
2021-06-11 22:35:17 +0800

    php7.2

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,7 +5,6 @@ namespace Recca0120\ParallelTest\Tests;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Router;
-use Illuminate\Support\Facades\Auth;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use Recca0120\ParallelTest\ParallelTestServiceProvider;
 
@@ -43,9 +42,9 @@ class TestCase extends BaseTestCase
     protected function defineRoutes($router): void
     {
         $router->match(['post', 'put', 'patch', 'options', 'delete'], '/auth/login', function (Request $request) {
-            return Auth::attempt($request->only('email', 'password'))
-                ? Auth::user()->setHidden(['email_verified_at', 'remember_token'])
-                : [];
+            $user = ['email' => 'recca0120@gmail.com', 'password' => 'password'];
+
+            return $user['email'] === $request->get('email') && $user['password'] === $request->get('password') ? $user : [];
         });
     }
 }
