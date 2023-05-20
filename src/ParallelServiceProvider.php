de57e6ab6bdad68bd3ec7fa526f69ca4196d5d4a
recca0120 <recca0120@gmail.com>
2023-05-20 11:11:41 +0800

    add fake sqlite db

--- src/ParallelServiceProvider.php
+++ src/ParallelServiceProvider.php
@@ -9,6 +9,15 @@ class ParallelServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        config([
+            'database.connections.laravel-parallel' => [
+                'driver' => 'sqlite',
+                'database' => database_path('laravel-parallel.sqlite'),
+                'prefix' => '',
+                'foreign_key_constraints' => true,
+            ],
+        ]);
+
         $this->commands([ParallelCommand::class]);
     }
 }
